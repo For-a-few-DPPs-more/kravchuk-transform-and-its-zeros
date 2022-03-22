9512317da82b21cf74b6a171b0bd71caa33b59b3
Guillaume Gautier <guillaume.gga@gmail.com>
2022-03-22 11:36:33 +0100

    format python files with black

--- src/kravtrans0/kravchuk_display.py
+++ src/kravtrans0/kravchuk_display.py
@@ -1,59 +1,71 @@
-import numpy             as np
-import numpy.random      as npr
-import scipy.special     as sps
-from matplotlib.pyplot   import figure
+import matplotlib as mpl
 import matplotlib.pyplot as plt
-import matplotlib        as mpl
+import numpy as np
+import numpy.random as npr
+import scipy.special as sps
+from matplotlib.pyplot import figure
 
 sapin = (0.0353, 0.3216, 0.1569)
 
-def signal_display(time_t,nsignal):
 
-    plt.plot(time_t,nsignal,color = sapin, linewidth = 1);
+def signal_display(time_t, nsignal):
+
+    plt.plot(time_t, nsignal, color=sapin, linewidth=1)
     plt.grid()
-    plt.xlabel('$t$ (s)')
+    plt.xlabel("$t$ (s)")
     plt.tight_layout()
 
-def planar_display(Kz, zt, zp, Nt = 500, Np = 500):
-    
-    thetas = np.linspace(1e-10,np.pi,Nt)
-    phis   = np.linspace(0,2*np.pi,Np)
-    
+
+def planar_display(Kz, zt, zp, Nt=500, Np=500):
+
+    thetas = np.linspace(1e-10, np.pi, Nt)
+    phis = np.linspace(0, 2 * np.pi, Np)
+
     # Display the spectrogram and its zeros
-    figure(figsize = (8,6));
-    plt.pcolormesh(phis, thetas,np.abs(Kz), shading = 'gouraud',cmap='gray')
-    plt.scatter(zp,zt,s = 15,color = 'mistyrose')
-    plt.xlabel(r'$\varphi$',fontsize = 45)
-    plt.ylabel(r'$\vartheta$',fontsize = 45)
+    figure(figsize=(8, 6))
+    plt.pcolormesh(phis, thetas, np.abs(Kz), shading="gouraud", cmap="gray")
+    plt.scatter(zp, zt, s=15, color="mistyrose")
+    plt.xlabel(r"$\varphi$", fontsize=45)
+    plt.ylabel(r"$\vartheta$", fontsize=45)
     # plt.xticks(np.pi/3*np.arange(7), ["$0$", "$\pi/3$", "$2\pi/3$", "$\pi$", "$4\pi/3$", "$5\pi/3$", "$2\pi$"]);
     # plt.yticks(np.pi/6*np.arange(7), ["$0$", "$\pi/6$", "$\pi/3$", "$\pi/2$", "$2\pi/3$", "$5\pi/6$", "$\pi$"]);
-    plt.xticks(np.pi*np.arange(3), ["$0$", "$\pi$", "$2\pi$"]);
-    plt.yticks(np.pi/2*np.arange(3), ["$0$", "$\pi/2$", "$\pi$"]);
+    plt.xticks(np.pi * np.arange(3), ["$0$", "$\pi$", "$2\pi$"])
+    plt.yticks(np.pi / 2 * np.arange(3), ["$0$", "$\pi/2$", "$\pi$"])
     plt.tight_layout()
 
-    
-def spherical_display(Kz, zt, zp, Nt = 500, Np = 500):
 
-    # maps of angles 
-    thetas = np.linspace(1e-10,np.pi,Nt)
-    phis   = np.linspace(0,2*np.pi,Np)
-    Thetas, Phis = np.meshgrid(thetas,phis,indexing='ij')
-                      
+def spherical_display(Kz, zt, zp, Nt=500, Np=500):
+
+    # maps of angles
+    thetas = np.linspace(1e-10, np.pi, Nt)
+    phis = np.linspace(0, 2 * np.pi, Np)
+    Thetas, Phis = np.meshgrid(thetas, phis, indexing="ij")
+
     # Cartesian coordinates of the unit sphere
-    XX         = np.sin(Thetas) * np.cos(Phis)
-    YY         = np.sin(Thetas) * np.sin(Phis)
-    ZZ         = np.cos(Thetas)
+    XX = np.sin(Thetas) * np.cos(Phis)
+    YY = np.sin(Thetas) * np.sin(Phis)
+    ZZ = np.cos(Thetas)
 
     # Kravchuk transform of white noise
-    test       = np.abs(Kz)
+    test = np.abs(Kz)
     tmin, tmax = test.min(), test.max()
-    test       = (test - tmin)/(tmax - tmin)
+    test = (test - tmin) / (tmax - tmin)
 
     # Plot the surface
-    fig = plt.figure(figsize=4*plt.figaspect(1.))
-    ax = fig.add_subplot(111, projection='3d')
-    ax.plot_surface(XX, YY, ZZ, rstride=1, cstride=1, zorder=-1, facecolors=mpl.cm.gray(test), shade = False);
-    ax.set_axis_off();  # Turn off the axis planes
+    fig = plt.figure(figsize=4 * plt.figaspect(1.0))
+    ax = fig.add_subplot(111, projection="3d")
+    ax.plot_surface(
+        XX,
+        YY,
+        ZZ,
+        rstride=1,
+        cstride=1,
+        zorder=-1,
+        facecolors=mpl.cm.gray(test),
+        shade=False,
+    )
+    ax.set_axis_off()
+    # Turn off the axis planes
 
     elev = -60
     azim = -145
@@ -63,12 +75,12 @@ def spherical_display(Kz, zt, zp, Nt = 500, Np = 500):
     xs = []
     ys = []
     zs = []
-    eps=1e-3 # used to shift zeros towards the viewer, and bypass a rendering bug on zorder
+    eps = 1e-3  # used to shift zeros towards the viewer, and bypass a rendering bug on zorder
 
     for i in range(len(zt)):
 
         # spherical coordinates
-        phi   = zp[i]
+        phi = zp[i]
         theta = zt[i]
 
         # Cartesian coordinates
@@ -76,13 +88,15 @@ def spherical_display(Kz, zt, zp, Nt = 500, Np = 500):
         y = np.sin(theta) * np.sin(phi)
         z = np.cos(theta)
 
-        for j in range(2,3):
+        for j in range(2, 3):
             npr.seed(j)
             n = npr.randn(3)
-            if np.dot(n, np.array([x,y,z]))>=0: # that is, if the zero is on the hemisphere we can see
+            if (
+                np.dot(n, np.array([x, y, z])) >= 0
+            ):  # that is, if the zero is on the hemisphere we can see
                 #            if x-y+z>=0: # that is, if the zero is on the hemisphere we can see
-                t = np.array([x,y,z]) + eps*n
-                xs.append(t[0]) # shift the zeros towards the viewer
+                t = np.array([x, y, z]) + eps * n
+                xs.append(t[0])  # shift the zeros towards the viewer
                 ys.append(t[1])
                 zs.append(t[2])
 
--- src/kravtrans0/white_noises.py
+++ src/kravtrans0/white_noises.py
@@ -1,62 +1,84 @@
-import numpy               as np
-import numpy.random        as npr
 import os
-from scipy.io                        import savemat, loadmat
 
-from chirp_signals          import the_chirp, the_white_noise, the_noisy_chirp
-from kravchuk_transform     import the_transform, the_zeros
-from kravchuk_display       import signal_display, planar_display, spherical_display
-from spherical_statistics   import the_distance, the_F_statistics, the_K_statistics, empirical_F, empirical_K
+import numpy as np
+import numpy.random as npr
+from chirp_signals import the_chirp, the_noisy_chirp, the_white_noise
+from kravchuk_display import planar_display, signal_display, spherical_display
+from kravchuk_transform import the_transform, the_zeros
+from scipy.io import loadmat, savemat
+from spatialstats import spatialStatsFromR
+from spherical_statistics import (
+    empirical_F,
+    empirical_K,
+    the_distance,
+    the_F_statistics,
+    the_K_statistics,
+)
+from stft_transform import stft_display, the_stft_transform, the_stft_zeros
 
-from stft_transform  import the_stft_transform, the_stft_zeros, stft_display
-from spatialstats    import spatialStatsFromR
 
-def noise_samples(N,alpha = 0.05,m=199, time_t=[],folder = 'samples'):
-    
+def noise_samples(N, alpha=0.05, m=199, time_t=[], folder="samples"):
+
     """
     generate m samples of complex white Gaussian noise and perform all required analysis for zero-based detection
     """
-    
-    #N+1: number of points in the white noise
-    #alpha: desired significance level of the test
-    #m:   number of realizations for the Monte Carlo envelope test
-    #folder: name of the folder in which the white noise samples are stored
-    
+
+    # N+1: number of points in the white noise
+    # alpha: desired significance level of the test
+    # m:   number of realizations for the Monte Carlo envelope test
+    # folder: name of the folder in which the white noise samples are stored
+
     # create a folder to save the white noise samples
-    if os.path.isdir('../'+folder) == False:
-        os.mkdir('../'+folder)
+    if os.path.isdir("../" + folder) == False:
+        os.mkdir("../" + folder)
     if len(time_t) == 0:
-        time_t     = np.arange(N+1)
-        
+        time_t = np.arange(N + 1)
+
     for n in np.arange(m):
 
         # sample white Gaussian noise
-        wnoise   = the_white_noise(N)
-        
+        wnoise = the_white_noise(N)
+
         # Kravchuk spectrogram analysis
         # compute its Kravchuk transform
-        Kn       = the_transform(wnoise)
+        Kn = the_transform(wnoise)
         # find the zeros
-        znt, znp = the_zeros(Kn,N)
+        znt, znp = the_zeros(Kn, N)
         # explored range of r
-        rs       = np.linspace(0,2*np.pi/np.sqrt(N),10**4)
+        rs = np.linspace(0, 2 * np.pi / np.sqrt(N), 10 ** 4)
         # compute the F-function
-        Fn,_     = the_F_statistics(znt,znp,rs)
+        Fn, _ = the_F_statistics(znt, znp, rs)
         # compute Ripley's K function
-        RipK,_   = the_K_statistics(znt,znp)
-        
+        RipK, _ = the_K_statistics(znt, znp)
+
         # Standard time-frequency analysis
         # compute the stft transform
-        Vn, fn   = the_stft_transform(wnoise, time_t)
-        # find its zeros 
-        vnt, vnf = the_stft_zeros(Vn,time_t,fn)
+        Vn, fn = the_stft_transform(wnoise, time_t)
+        # find its zeros
+        vnt, vnf = the_stft_zeros(Vn, time_t, fn)
         # compute the spatial statistics of the STFT
-        r_K, KR, r_F, FR = spatialStatsFromR(time_t,fn,vnt,vnf)
+        r_K, KR, r_F, FR = spatialStatsFromR(time_t, fn, vnt, vnf)
 
         # Save the data
-        mdict = {"N" : N, "wnoise" : wnoise, "time_t" : time_t, "Kn" : Kn, "znt" : znt, "znp" : znp,
-                "rs" : rs, "Fn" : Fn, "RipK" : RipK, "Vn" : Vn, "fn" : fn, "vnt" : vnt, "vnf" : vnf,
-                "r_K" : r_K, "KR" : KR, "r_F" : r_F, "FR" : FR}
-        savemat('../samples/noise_'+str(n)+'.mat',mdict)
-        
+        mdict = {
+            "N": N,
+            "wnoise": wnoise,
+            "time_t": time_t,
+            "Kn": Kn,
+            "znt": znt,
+            "znp": znp,
+            "rs": rs,
+            "Fn": Fn,
+            "RipK": RipK,
+            "Vn": Vn,
+            "fn": fn,
+            "vnt": vnt,
+            "vnf": vnf,
+            "r_K": r_K,
+            "KR": KR,
+            "r_F": r_F,
+            "FR": FR,
+        }
+        savemat("../samples/noise_" + str(n) + ".mat", mdict)
+
     return alpha, m, folder
